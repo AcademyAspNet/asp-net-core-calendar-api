c12c75b2bc964f891215d9d149c016b905228b15
AcademyAspNet <elprofesor.espana@gmail.com>
2025-08-18 12:55:54 +0300

    Added custom middleware

---
+++ Middleware/ApiKeyValidationMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace MiddlewareExampleWebAPI.Middleware
+{
+    public class ApiKeyValidationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _validApiKey = "calendar";
+
+        public ApiKeyValidationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string? apiKey = context.Request.Headers["X-API-Key"];
+
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("API ключ не указан!");
+
+                return;
+            }
+
+            if (apiKey != _validApiKey)
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Некорректный API ключ");
+
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+
+    public static class ApiKeyValidationMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseApiKeyValidation(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ApiKeyValidationMiddleware>();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using MiddlewareExampleWebAPI.Middleware;
+using System.Diagnostics;
+using System.Net;
+
 namespace MiddlewareExampleWebAPI
 {
     public class Program
@@ -9,6 +13,25 @@ namespace MiddlewareExampleWebAPI
             builder.Services.AddControllers();
 
             var app = builder.Build();
+
+            app.UseApiKeyValidation();
+
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine("Начинаем обработку запроса (Middleware 1)...");
+                await next(context);
+                Console.WriteLine("Заканчиваем обработку запроса (Middleware 1)...");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine("Начинаем обработку запроса (Middleware 2)...");
+                await next(context);
+                Console.WriteLine("Заканчиваем обработку запроса (Middleware 2)...");
+            });
+
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
 
             app.MapControllers();
 
