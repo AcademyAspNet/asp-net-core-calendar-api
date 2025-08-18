4dc40bb0d4b7b37dd5320e4ce6afb85206886813
AcademyAspNet <elprofesor.espana@gmail.com>
2025-08-18 11:21:52 +0300

    Add project files.

---
+++ Controllers/CalendarController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiddlewareExampleWebAPI.Controllers
+{
+    [ApiController]
+    [Route("/calendar")]
+    public class CalendarController : ControllerBase
+    {
+        [HttpGet("date")]
+        public IActionResult GetCurrentDate()
+        {
+            string currentDate = DateTime.Now.ToShortDateString();
+            return Content(currentDate);
+        }
+
+        [HttpGet("time")]
+        public IActionResult GetCurrentTime()
+        {
+            string currentTime = DateTime.Now.ToShortTimeString();
+            return Content(currentTime);
+        }
+    }
+}
